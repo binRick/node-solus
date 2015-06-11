c1d0512e6b711f81e83649765728071f9126525f
binRick <rapidvps@gmail.com>
2015-06-11 13:56:57 -0400

    new

--- plugins/solus.js
+++ plugins/solus.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 module.exports.Events = {
     createVM: {
         validate: function(reqJson) {
-            return reqJson.meta.solusvmid == 0 && reqJson.config.apiType == 'openvz' && reqJson.meta.ctid == '' && reqJson.service.desserv.length > 0;
+            return reqJson.meta.solusvmid == 0 && reqJson.config.apiType == 'openvz' && reqJson.meta.ctid == '' && reqJson.service.desserv.length > 0 && reqJson.client.username && reqJson.client.username.length > 6;
         },
         setup: function(reqJson, cb) {
             var Setup = {
@@ -12,6 +12,11 @@ module.exports.Events = {
                 hostname: reqJson.service.desserv,
                 username: reqJson.client.username,
                 template: reqJson.config.apiTemplate,
+                ips: reqJson.config.apiTemplate,
+                memory: reqJson.config.apiTemplate,
+                swap: reqJson.config.apiTemplate,
+                cpus: reqJson.config.apiTemplate,
+                disk: reqJson.config.apiTemplate,
             };
             if (reqJson.service.password && reqJson.service.password.length > 6)
                 Setup.password = reqJson.service.password;
