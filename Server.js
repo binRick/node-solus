02ef29577c786082591b9286f73083ea7fd917e0
binRick <rapidvps@gmail.com>
2015-06-14 13:20:04 -0400

    new

--- Server.js
+++ Server.js
@@ -2,20 +2,32 @@
 
 
 var express = require('express'),
+morgan = require('morgan'),
     fs = require('fs'),
+c = require('chalk'),
     _ = require('underscore'),
     app = new express(),
     multer = require('multer');
+SolusRouter = require('./Router');
+
+
 
 app.use(express.static(__dirname + '/public_html'));
 
 app.use(multer());
+//var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+//app.use(morgan('combined', {stream: accessLogStream}))
 
-var SolusRouter = require('./node-solus/Router');
 
 app.get('/', function(req, res) {
     res.end('asd');
 });
+
+app.post('/api/*', function(req, res, next){	
+//	console.log(c.green(JSON.parse(req.body.data)));
+	next();
+});
+
 app.post('/api/createVM', SolusRouter.Create);
 app.post('/api/cancelVM', SolusRouter.Cancel);
 app.post('/api/suspendVM', SolusRouter.Suspend);
